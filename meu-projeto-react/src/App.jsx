3d626010e1a66c08aae396bfaa36e22ba2cb4ebe
cahves1 <athoschaveslopesdealmeida@gmail.com>
2026-02-09 17:30:37 -0300

    09/02/2026 - Reviews Exercise

--- meu-projeto-react/src/App.jsx
+++ meu-projeto-react/src/App.jsx
@@ -1,15 +1,12 @@
 import './index.css'
-import List from './List'
-import { toursData } from './data'
+import Review from './Reviews'
 
 function App() {
   return (
     <>
-      <header>
-        <h1>Our Tours</h1>
-        <div className="title_footer"></div>
-      </header>
-      <List tours={toursData} />
+      <main className='main_container'>
+        <Review/>
+      </main>
     </>
   )
 }
--- meu-projeto-react/src/data.js
+++ meu-projeto-react/src/data.js
@@ -1,37 +1,40 @@
-export const toursData = [
+const data = [
   {
-    id: "rec6d6T3q5EBIdCfD",
-    name: "Best of Paris in 7 Days Tour",
-    info: "Paris is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of the City of Light: the masterpiece-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and extravagant Palace of Versailles.",
-    image: "https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?w=1200&q=90",
-    price: "1,995"
+    id: 1,
+    name: 'Susan Smith',
+    job: 'web developer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    text:
+      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
   {
-    id: "recIwxrvU9HfJR3B4",
-    name: "Best of Ireland in 14 Days Tour",
-    info: "Rick Steves' Best of Ireland tour kicks off with the best of Dublin, followed by Ireland's must-see historical sites, charming towns, music-filled pubs, and seaside getaways — including Kinsale, the Dingle Peninsula, the Cliffs of Moher, the Aran Islands, Galway, Connemara, Giant's Causeway, and the compelling city of Belfast.",
-    image: "https://images.unsplash.com/photo-1518509562904-e7ef99cdcc86?w=1200&q=90",
-    price: "3,895"
+    id: 2,
+    name: 'Anna Johnson',
+    job: 'Web Designer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+    text:
+      'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
   },
   {
-    id: "recJLWcHScdUtI3ny",
-    name: "Best of Salzburg & Vienna in 8 Days Tour",
-    info: "Let's go where classical music, towering castles, and the-hills-are-alive scenery welcome you to the gemütlichkeit of Bavaria and opulence of Austria's Golden Age. Your Rick Steves guide will bring this region's rich history and culture to life in festive Munich, Baroque Salzburg, sparkling Lake Hallstatt, monastic Melk, the blue Danube, and royal Vienna.",
-    image: "https://images.unsplash.com/photo-1575367439058-6096bb9cf5e2?w=1200&q=90",
-    price: "2,695"
+    id: 3,
+    name: 'Peter Jones',
+    job: 'intern',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    text:
+      'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
   },
   {
-    id: "recK2AOoVhIHPLUwn",
-    name: "Best of Rome in 7 Days Tour",
-    info: "Our Rome tour serves up Europe's most intoxicating brew of dazzling art, earth-shaking history, and city life with style. On this Rome vacation, your tour guide will resurrect the grandeur of ancient Rome's Colosseum, Forum, Pantheon, and nearby Ostia Antica.",
-    image: "https://images.unsplash.com/photo-1525874684015-58379d421a52?w=1200&q=90",
-    price: "2,095"
+    id: 4,
+    name: 'Bill Anderson',
+    job: 'the boss',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    text:
+      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
   },
-  {
-    id: "receAEzz86KzW2gvH",
-    name: "Best of Poland in 10 Days Tour",
-    info: "Starting in the colorful port city of Gdańsk, you'll escape the crowds and embrace the understated elegance of ready-for-prime-time Poland for 10 days. With an expert Rick Steves guide at your side, you'll experience mighty Malbork castle, the colorful medieval town of Toruń, Poland's contemporary capital of Warsaw, the spiritual Jasna Góra Monastery, and charming Kraków.",
-    image: "https://images.unsplash.com/photo-1585016495481-91613a3ab1bc?w=1200&q=90",
-    price: "2,595"
-  }
-]
+];
+
+export default data;
