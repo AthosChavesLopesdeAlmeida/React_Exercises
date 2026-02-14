00993cb7e2c9deebfd2f83faccad74db6b750206
cahves1 <athoschaveslopesdealmeida@gmail.com>
2026-02-14 13:25:44 -0300

    14/02/2026 - Color Generator Exercise

--- meu-projeto-react/src/App.jsx
+++ meu-projeto-react/src/App.jsx
@@ -1,41 +1,55 @@
+import React, { useState } from 'react'
+import SingleColor from './SingleColor'
 import './index.css'
-import { useState } from "react"
-import data from './data'
+
+import Values from 'values.js'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [text, setText] = useState([])
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false)
+  const [list, setList] = useState(new Values('#f15025').all(10))
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    let amount = parseInt(count);
-    
-    if (count <= 0) {
-      amount = 1
+    try {
+      let colors = new Values(color).all(10)
+      setList(colors)
+    } catch (error) {
+      setError(true)
+      console.log(error)
     }
-
-    if (count > 8) {
-      amount = 8
-    }
-
-    setText(data.slice(0, amount))
   }
 
   return (
-    <section>
-      <h1>TIRED OF BORING LOREM IPSUM?</h1>
-      <form className="lorem_form" onSubmit={handleSubmit}>
-        <label htmlFor="amount">Paragraphs: </label>
-        <input type="number" name="amount" id="amount" 
-        onChange={(e) => setCount(e.target.value)} className='amount_input'/>
-        <button className="btn">Generate</button>
-      </form>
-      <article>
-        {text.map((item, index) => {
-          return <p key={index}>{item}</p>
+    <>
+      <section className='container'>
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder='#f15025'
+            className={`${error ? 'error' : null}`}
+          />
+          <button className='btn' type='submit'>
+            submit
+          </button>
+        </form>
+      </section>
+      <section className='colors'>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          )
         })}
-      </article>
-    </section>
+      </section>
+    </>
   )
 }
 
